4e53b5ab55cf1117506010196b840393821a159b
Sara <saraahmad1114@yahoo.com>
2016-10-20 14:25:23 -0400

    changes

---
+++ swift-captain-morgans-relationships-lab/DataStore.swift
@@ -0,0 +1,14 @@
+//
+//  DataStore.swift
+//  swift-captain-morgans-relationships-lab
+//
+//  Created by Flatiron School on 10/20/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class DataStore{
+
+
+}
