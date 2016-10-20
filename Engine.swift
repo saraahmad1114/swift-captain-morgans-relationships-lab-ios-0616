ea1315401921afd154d5c423837f18be5e5f9f65
Sara <saraahmad1114@yahoo.com>
2016-10-20 14:33:40 -0400

    changes

---
+++ Engine.swift
@@ -0,0 +1,17 @@
+//
+//  Engine.swift
+//  swift-captain-morgans-relationships-lab
+//
+//  Created by Flatiron School on 10/20/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Engine: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
---
+++ Ship.swift
@@ -0,0 +1,17 @@
+//
+//  Ship.swift
+//  swift-captain-morgans-relationships-lab
+//
+//  Created by Flatiron School on 10/20/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Ship: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
